d31d21e3375547c1d4076804778bfb2666a50cb4
Tiago <tiagofccarvalho13@gmail.com>
2023-10-17 22:22:17 +0100

    Finished Exercise 5

---
+++ Ficha P02/EX5/complex_new.c
@@ -0,0 +1,7 @@
+#include "complex.h"
+complex complex_new(double x, double y) {
+complex z;
+z.x = x;
+z.y = y;
+return z;
+}
---
+++ Ficha P02/EX5/complex_sub.c
@@ -0,0 +1,6 @@
+#include "complex.h"
+complex complex_sub(complex z, complex w){
+complex r;r.x = z.x - w.x;
+r.y = z.y - w.y;
+return r;
+}
