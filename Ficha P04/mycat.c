eedf15bedb3733fd92fced6120357185d421eaaa
Tiago <tiagofccarvalho13@gmail.com>
2023-10-25 08:44:19 +0100

    Made exercise 1, changing mycat to read several files

--- Ficha P04/mycat.c
+++ Ficha P04/mycat.c
@@ -6,15 +6,17 @@
 #include <fcntl.h>
 #define BUFFER_SIZE 1024
 int main(int argc, char* argv[]) {
-if (argc != 2) {
+if (argc < 2) {
 printf("usage: cat filename\n");
 exit(EXIT_FAILURE);
 }
-int fd = open(argv[1], O_RDONLY);
+for (int i = 1; i < argc; i++) {
+int fd = open(argv[i], O_RDONLY);
 if (fd == -1) {
 printf("error: cannot open %s\n", argv[1]);
 exit(EXIT_FAILURE);
 }
+
 char buffer[BUFFER_SIZE];
 int nbytes = read(fd, buffer, BUFFER_SIZE);
 while (nbytes > 0) {
@@ -22,5 +24,7 @@ write(STDOUT_FILENO, buffer, nbytes);
 nbytes = read(fd, buffer, BUFFER_SIZE);
 }
 close(fd);
+}
+
 exit(EXIT_SUCCESS);
 }
