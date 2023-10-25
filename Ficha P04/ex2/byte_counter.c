95f78163c6ba0e053a4681bc6580cd1b52b34d7a
Tiago <tiagofccarvalho13@gmail.com>
2023-10-25 09:11:08 +0100

    Added the multiple insertion and total size counter on exercise 2

--- Ficha P04/ex2/byte_counter.c
+++ Ficha P04/ex2/byte_counter.c
@@ -3,16 +3,25 @@
 #include <stdlib.h>
 int main(int argc, char* argv[]) {
 struct stat info;
-if (argc != 2) {
+if (argc < 2) {
 fprintf(stderr, "usage: %s file\n", argv[0]);
 exit(EXIT_FAILURE);
 }
-int retv = stat(argv[1], &info);
+
+int counter_size = 0;
+int counter_blocks = 0 ;
+
+for (int i = 1; i < argc; i++) {
+int retv = stat(argv[i], &info);
 if (retv == -1) {
-fprintf(stderr, "fsize: Can’t stat %s\n", argv[1]);
+fprintf(stderr, "fsize: Can’t stat %s\n", argv[i]);
 exit(EXIT_FAILURE);
 }
 printf("%s size: %d bytes, disk_blocks: %d\n",
-argv[1], (int)info.st_size, (int)info.st_blocks);
-exit(EXIT_SUCCESS);
+argv[i], (int)info.st_size, (int)info.st_blocks);
+counter_size += (int)info.st_size;
+counter_blocks += (int)info.st_blocks;
 }
+printf("total size: %d bytes,  counter_size, total disc blocks: %d\n", counter_size, counter_blocks);
+exit(EXIT_SUCCESS);
+} 
